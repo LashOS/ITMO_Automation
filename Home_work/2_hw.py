2458a35a14774e7400cc822f40651e86d2d37d1c
LashOS <162912904+LashOS@users.noreply.github.com>
2024-03-13 19:40:56 +0300

    FIrst Commit

--- Home_work/2_hw.py
+++ Home_work/2_hw.py
@@ -1,13 +1,10 @@
-a: int = 1
-b: float = 2.2
-c: str = 'Privet'
-d: list = [1, 2, 3]
-e: bool = True
-print(a, "относится к типу", type (a))
-print(b, "относится к типу", type (b))
-print(c, "относится к типу", type (c))
-print(d, "относится к типу", type (d))
-print(e, "относится к типу", type (e))
+def task_1(a: int, b: float, c: str, d: list, e: bool):
+    print(a, "относится к типу", type (a))
+    print(b, "относится к типу", type (b))
+    print(c, "относится к типу", type (c))
+    print(d, "относится к типу", type (d))
+    print(e, "относится к типу", type (e))
+task_1(a = 1, b = 2.2, c = 'Privet', d = [1, 2, 3], e = True)
 def task_2(a = [1,2,3,4,5,8,13,21])->list:
     return a[0:3]
 print(task_2())
