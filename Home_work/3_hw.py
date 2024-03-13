32f985cb24ce303a1c016024ffd40ce2722e5bbe
LashOS <162912904+LashOS@users.noreply.github.com>
2024-03-13 21:31:24 +0300

    FIrst Commit

---
+++ Home_work/3_hw.py
@@ -0,0 +1,50 @@
+def task_1(a, b):
+    if a > b:
+        print(a)
+    elif a < b:
+        print(b)
+    elif a == b:
+        print("Равны")
+    else:
+        print("Введите корректное число")
+task_1(a = 1, b = 2)
+
+def task_2(a, b):
+    if a - b == 135 or b - a == 135:
+        print("Yes")
+    else:
+        print("No")
+task_2(a = 235, b = 100)
+def task_3(a, b = ("Зима", "Весна", "Лето", "Осень")):
+    if a == 12 and a >= 2:
+        print(b[0])
+    elif a in range(3, 6):
+        print(b[1])
+    elif a in range(6, 9):
+        print(b[2])
+    elif a in range(9, 12):
+        print(b[3])
+task_3(a = 6)
+
+def task_4(a, b, c):
+    if a > 10 and b > 10 and c > 10:
+        print("yas")
+    else:
+        print("No")
+task_4(a = 10, b = 11, c = 12)
+
+def task_5(a, pos_count = 0, neg_count = 0):
+    for num in a:
+        if num >= 0:
+            pos_count += 1
+        else:
+            neg_count += 1
+    print("Положительных чисел в списке ", pos_count)
+    print("Отрицательных чисел в списке ", neg_count)
+task_5(a = [23, -34, 34, -43, 324])
+
+def task_6(a, b, month = 29):
+    print((a * (month * 12)) + (b * month))
+task_6(a = 2, b = 4)
+
+
--- python_traning/task_3_list.py
+++ python_traning/task_3_list.py
@@ -1,11 +1,11 @@
-a = [5, 10, 15, 29, 25, 30, 15, 40]
-print("a[2] = ", a[0:3])
+#a = [5, 10, 15, 29, 25, 30, 15, 40]
+#print("a[2] = ", a[0:3])
 
-print("a[0:4]= ", a[0:4])
+#print("a[0:4]= ", a[0:4])
 
-b = [11, 12, 13]
-b[2] = 4
-print(b)
+# = [11, 12, 13]
+#b[2] = 4
+#print(b)
 
 test_list = ["odin", "Dva", "Tri", "4etire", "pat'"]
 
@@ -14,6 +14,6 @@ for elen in test_list:
 
 print(len(test_list))
 
-test_list.append("shest")
-print(test_list)
+#test_list.append("shest")
+#print(test_list)
 
